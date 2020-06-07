6e02e849bd41226d3ac214f75892738c8c9c8a2e
LWRGitHub <loganreynoldslwr@gmail.com>
2020-06-07 15:53:09 -0700

    Added files and set up javascript alert

---
+++ javascript/index.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+var time = 3000;
+showSlides();
+
+
+//CTD alert
+alert("Welcome to a new journey and my new site!");
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  time = 3000;
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  time = 3000;
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, time); // Change image every 2 seconds
+}
